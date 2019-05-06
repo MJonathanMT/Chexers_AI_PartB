ebd03915d1b9638868f5fe96b9f3ab6b9ad7968a
MJonathanMT <48747653+MJonathanMT@users.noreply.github.com>
2019-05-06 12:05:44 +1000

    Add files via upload

--- daboiz/player.py
+++ daboiz/player.py
@@ -27,14 +27,40 @@ class Player:
         actions.
         """
         # TODO: Decide what action to take.
-        return ("PASS", None)
+        exit_move = False
+        action = ("PASS", None)
+        for piece in self.pieces:
+            if piece in self.goals:
+                action = ("EXIT", piece)
+                self.pieces.remove(piece)
+                exit_move = True
+                return action
 
+        if not exit_move:
+            # Get the best move possible for each piece
+            best_moves = get_moves(self, board_dict, dist_dict, adj_dict)
+
+            # Get the best jump possible for each piece
+            best_jumps = get_jumps(dist_dict, adj_dict, blockers, pieces)
+
+            # Get the best action possible for each piece
+            final_moves = final_movements(dist_dict, best_moves, best_jumps)
+
+            # Choose the best piece to move
+            final_move = get_piece(dist_dict, final_moves)
+            if final_move == ():
+                return action
+            if final_move[2] == 'move':
+                action = ("MOVE", (str(final_move[0]), str(final_move[1])))
+            elif final_move[2] == 'jump':
+                action = ("JUMP", (str(final_move[0]), str(final_move[1])))
+        return action
 
     def update(self, colour, action):
         """
         This method is called at the end of every turn (including your player’s 
         turns) to inform your player about the most recent action. You should 
-        use this opportunity to maintain your internal representation of the
+        use this opportunity to maintain your internal representation of the 
         game state and any other information about the game you are storing.
 
         The parameter colour will be a string representing the player whose turn
@@ -48,4 +74,169 @@ class Player:
         (or pass) for the player colour (your method does not need to validate 
         the action/pass against the game rules).
         """
+
         # TODO: Update state representation in response to action.
+
+
+def get_goals(colour):
+    """
+    This function returns a list of all the available goals
+     that the pieces can exit from
+    :param colour: The colour of the player's pieces given by the json file
+    :return: A list of all the available goals
+    """
+    end_points = []
+    if colour == 'red':
+        end_points = [(3, -3), (3, -2), (3, -1), (3, 0)]
+    elif colour == 'blue':
+        end_points = [(0, -3), (-1, -2), (-2, -1), (-3, 0)]
+    elif colour == 'green':
+        end_points = [(-3, 3), (-2, 3), (-1, 3), (0, 3)]
+
+    # Returns the final list of end_points/goals
+    return end_points
+
+
+def get_moves(self, board_dict, dist_dict, adj_dict):
+    best_moves = {}
+    for piece in self.pieces:
+        movement = []
+        shortest_dist = 0
+        best_move = ()
+        for next_move in adj_dict[piece]:
+            # Skips move if there is another piece in front of it or
+            # if there is a blocked hex
+            if next_move in board_dict:
+                continue
+
+            # the best move is where the distance of the next
+            # location is the closest to the goal
+            if shortest_dist == 0:
+                shortest_dist = dist_dict[next_move]
+                best_move = next_move
+            elif dist_dict[next_move] < shortest_dist:
+                shortest_dist = dist_dict[next_move]
+                best_move = next_move
+
+        # if the best move makes the piece go backward
+        # (Worsen the piece condition) remove the best move instead
+        if best_move != () and dist_dict[piece] <= dist_dict[best_move]:
+            best_move = ()
+
+        # Each value of the position contains the current position
+        # and next position
+        movement.append(piece)
+        movement.append(best_move)
+        best_moves[piece] = movement
+
+    return best_moves
+
+
+def get_jumps(self, board_dict, dist_dict, adj_dict):
+    best_jumps = {}
+    for piece in self.pieces:
+        shortest_dist = 0
+        jumping = []
+        best_jump = ()
+        for next_move in adj_dict[piece]:
+            # Skips if the next move is an empty hex,
+            # this means its not jumpable
+            if next_move not in board_dict:
+                continue
+            q_move = (next_move[0] - piece[0])*2
+            r_move = (next_move[1] - piece[1])*2
+            next_jump = (piece[0] + q_move, piece[1] + r_move)
+            # Skips the next jump if the next jump location is
+            # another piece, a block or it isn't within the board
+            if next_jump not in adj_dict[next_move] or next_jump in board_dict:
+                continue
+
+            # the best jump is where the distance of the next
+            # location is the closest to the goal
+            if shortest_dist == 0:
+                shortest_dist = dist_dict[next_jump]
+                best_jump = next_jump
+            elif dist_dict[next_jump] < shortest_dist:
+                shortest_dist = dist_dict[next_jump]
+                best_jump = next_jump
+
+        # if the best jump makes the piece go backward
+        # (Worsen the piece condition) remove the best jump instead
+        if best_jump != () and dist_dict[piece] <= dist_dict[best_jump]:
+            best_jump = ()
+
+        # Each value of the position contains the current position
+        # and next position
+        jumping.append(piece)
+        jumping.append(best_jump)
+        best_jumps[piece] = jumping
+
+    return best_jumps
+
+
+def final_movements(dist_dict, best_moves, best_jumps):
+    """
+    This function chooses whether each piece should do a move or jump action
+    :param dist_dict: Dictionary containing the distance to the nearest goal
+    :param best_moves: Dictionary containing the best move of each piece
+    :param best_jumps: Dictionary containing the best jump of each piece
+    :return: The best action each piece should take whether its a move or a jump
+    """
+    final_moves = {}
+    for piece in best_moves:
+        move = best_moves[piece][1]
+        # If there are no jump action, make the best action the move action
+        if best_jumps[piece][1] == ():
+            final_moves[piece] = best_moves[piece]
+            final_moves[piece].append('move')
+            continue
+
+        # If there are no move action, make the best action the jump action
+        if move == ():
+            final_moves[piece] = best_jumps[piece]
+            final_moves[piece].append('jump')
+            continue
+
+        # Choose the action based on the next location the piece lands in.
+        # Choose the action that pushes the piece closer to the goal
+        jump = best_jumps[piece][1]
+        if dist_dict[move] < dist_dict[jump]:
+            final_moves[piece] = best_moves[piece]
+            final_moves[piece].append('move')
+        else:
+            final_moves[piece] = best_jumps[piece]
+            final_moves[piece].append('jump')
+
+    return final_moves
+
+
+def get_piece(dist_dict, final_moves):
+    """
+    This function choose the best piece to move amongst the piece on the board
+    :param dist_dict: Dictionary containing the distance to the nearest goal
+    :param final_moves: Dictionary containing the best action for each piece
+    :return: The piece that is best to move given the current board position
+    """
+    final_move = []
+    dist = 0
+    for piece in final_moves:
+        # Skip this piece if there aren't any good action available
+        if final_moves[piece][1] == ():
+            continue
+
+        if dist == 0:
+            dist = dist_dict[piece]
+            final_move = final_moves[piece]
+
+        # Choose the piece that is further away from the goal
+        elif dist_dict[piece] > dist:
+            dist = dist_dict[piece]
+            final_move = final_moves[piece]
+
+        # If the same distance away from the goal,
+        # choose piece with the jump action
+        elif dist_dict[piece] == dist:
+            if final_moves[piece][2] == 'jump':
+                final_move = final_moves[piece]
+
+    return final_move
