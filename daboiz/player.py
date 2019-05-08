7d12f783659ac36d7602755c4ea448df38717316
yho4 <zyyho97@gmail.com>
2019-05-08 17:21:58 +1000

    bug fix

--- daboiz/player.py
+++ daboiz/player.py
@@ -64,7 +64,7 @@ def find_eaten(before_jump, after_jump):
     q_move = (after_jump[0] - before_jump[0])/2
     r_move = (after_jump[1] - before_jump[1])/2
 
-    eaten = (before_jump + q_move, before_jump + r_move)
+    eaten = (before_jump[0] + q_move, before_jump[1] + r_move)
     return eaten
 
 
